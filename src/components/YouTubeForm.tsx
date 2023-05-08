09f3a74021c20d544b78295e59b5b2aad61b89b0
itsparasbisht <iamparasbisht@gmail.com>
2023-05-08 20:36:13 +0530

    form submit

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,17 +1,31 @@
+import { useForm } from "react-hook-form";
+
+type FormValues = {
+  username: string;
+  email: string;
+  channel: string;
+};
+
 export const YouTubeForm = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit = (data: FormValues) => {
+    console.log("form submitted", data);
+  };
+
   return (
     <div>
       <h1>YouTube Form</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
